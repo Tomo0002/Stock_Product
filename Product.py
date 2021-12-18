a35f4acaeaa708ed2e8abf49be3a2d26c72bc2b5
[Tomo0002] <[arimototomonori@gmail.com]>
2021-12-18 12:21:20 +0900

    main

--- Product.py
+++ Product.py
@@ -63,7 +63,6 @@ print(f"平均売上個数{Ave_Sales.round(2)}")
 # 結果を四捨五入する
 # print(data.describe().round(2))
 
-# グラフ表示 (https://www.webdevqa.jp.net/ja/python/visual-studio-code%E8%87%AA%E4%BD%93%E3%81%AB%E3%82%B0%E3%83%A9%E3%83%95%E3%82%92%E8%A1%A8%E7%A4%BA%E3%81%99%E3%82%8B%E6%96%B9%E6%B3%95%E3%81%AF%EF%BC%9F/838766720/)
 
 
 
