ffb3beaf481acd17094a9e33c0c5a27278ca0278
Tomo0003 <arimototomonori@gmai.com>
2021-12-31 13:48:25 +0900

    main

---
+++ Sale_DeepLearing.py
@@ -0,0 +1,19 @@
+from __future__ import print_function
+
+import pandas as pd
+from pandas import Series,DataFrame
+
+from sklearn import svm
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+import keras
+from keras.datasets import fashion_mnist
+from keras.models import Sequential
+from keras.layers import Dense, Dropout
+from keras.optimizers import RMSprop
+from keras.optimizers import Adam
+
