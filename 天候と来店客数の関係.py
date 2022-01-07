3151b5d74bd24a555171e802895cea96562bc05e
Tomo0003 <arimototomonori@gmail.com>
2022-01-07 12:29:04 +0900

    機能修正

--- 天候と来店客数の関係.py
+++ 天候と来店客数の関係.py
@@ -3,3 +3,5 @@ import numpy as np
 
 
 df = pd.read_csv("Data\神戸_気象データ.csv")
+df_1 = pd.read_csv("RESAS\(三ノ宮駅)兵庫県の滞在人口の動向_推定居住地ごとの2019年同週比の推移.csv", encoding="utf-8")
+df_2 = pd.read_csv("RESAS\(三宮駅)兵庫県の滞在人口の動向_推定居住地ごとの2019年同週比の推移.csv", encoding = "utf-8")
