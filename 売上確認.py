a637a06186c63f444f374da75e39d2e5a3ed4794
Tomo0003 <arimototomonori@gmail.com>
2022-01-07 11:24:32 +0900

    main

--- 売上確認.py
+++ 売上確認.py
@@ -22,7 +22,7 @@ df = pd.read_csv("Data\売上データ.csv")
 
 
 # 条件指定・抽出
-# print(df[df["Number_Of_Stock"] > 100])
+# print(df[df["在庫量"] > 100])
 
 # 条件分岐
 # Stocksituation = [
@@ -33,7 +33,7 @@ df = pd.read_csv("Data\売上データ.csv")
 # ]
 
 # choicelist = ['欠品','注文', '注意', '安全']
-# df["Stock_Status"] = np.select(Stocksituation, choicelist, default="Non")
+# df["在庫状況"] = np.select(Stocksituation, choicelist, default="Non")
 
 print(df)
 
@@ -45,6 +45,11 @@ print(f"更新時間: {datetime.datetime.now()}")
 # print(df_1)
 # Ave_Max_Temple_Osa_Des = average(df_temperature_Osa["最高気温"])
 # print(f"12月の大阪平均気温(最高){Ave_Max_Temple_Osa_Des.round(2)}℃")
+MAX_TEMPERATURE = max(df["最高気温"])
+print(f"最高気温:{MAX_TEMPERATURE}℃")
+
+AVERAGE_TEMPERATURE = average(df["最高気温"])
+print(f"平均気温(最高):{AVERAGE_TEMPERATURE}℃")
 
 # 計算
 Sum_Sales = sum(df["売上個数"])
