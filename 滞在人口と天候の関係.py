22028a75eead26c689320ef50d9e02e0f73e67d9
Tomo0003 <arimototomonori@gmail.com>
2022-01-13 11:12:06 +0900

    改善

--- 滞在人口と天候の関係.py
+++ 滞在人口と天候の関係.py
@@ -1,23 +1,34 @@
+from os import terminal_size
 import pandas as pd
+from scipy.sparse.construct import random
 
 from sklearn.model_selection import train_test_split
 from sklearn.neural_network import MLPClassifier
 
-# 出典：「全国の人流オープンデータ」（国土交通省）（https://www.geospatial.jp/ ckan/dataset/mlit-1km-fromto）
-df = pd.read_csv("Data\滞在人口データ\兵庫県_滞在人口1_2月.csv")
 
+# 出典：「全国の人流オープンデータ」（国土交通省）（https://www.geospatial.jp/ ckan/dataset/mlit-1km-fromto）
 # 「全国の人流オープンデータ」（国土交通省）（https://www.geospatial.jp/ckan/dataset/mlit-1km-fromto）を加工して作成
 # メッシュID:28110 神戸市中央区　兵庫県 28 
-df_Kobe = df.query('citycode == 28110' and 'timezone == 1')
 
-# メッシュID:28204　西宮市 兵庫県 28 
-df_Nisinomiya = df.query('citycode == 28240' and 'timezone == 1')
+df = pd.read_csv("Data\売上データ1月.csv", header=1)
 
-# メッシュID:28207 伊丹市　兵庫県 28 
-df_Itami = df.query('citycode == 28207' and 'timezone == 1')
+arr = df.values
 
-# メッシュID:28219 三田市　兵庫県 28
-df_Sanda = df.query('citycode == 28219' and 'timezone == 1')
+X = arr[:,:3]
+y = arr[:,1]
+
+print(y)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3, random_state= 0)
+
+model = MLPClassifier()
+
+model.fit(X_train, y_train)
+
+print(model.score(X_test, y_test))
+
+# 使用するデータの読み込み 
+
 
 # ある行の抽出
 # print(df.loc[667,['citycode']])
