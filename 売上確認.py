8ef30643e414221c23f1b03069c59f6ca30ffbdc
Tomo0003 <arimototomonori@gmail.com>
2022-01-16 15:58:13 +0900

    変更

--- 売上確認.py
+++ 売上確認.py
@@ -1,9 +1,12 @@
+from unicodedata import category
 import pandas as pd
+import numpy as np
 import datetime
 from numpy.lib.function_base import average
+import os
 
 # データ取得
-df = pd.read_csv("Data\売上データ.csv")
+df = pd.read_csv("Data\売上データ全体.csv")
 
 # 気象データを取得　(作成中)
 # url = "https://www.data.jma.go.jp/obd/stats/data/mdrr/tem_rct/alltable/mxtemsadext00_rct.csv"
@@ -12,10 +15,12 @@ df = pd.read_csv("Data\売上データ.csv")
 # ↑　https://ibuki-study.net/csv_from_url_by_python/
 
 
+    
 # 条件指定
 # print(df[df["在庫量"] > 100])
 
 # 条件分岐
+
 Stocksituation = [
     (df["在庫量"] == 0),
     (df["在庫量"] < 24),
@@ -25,7 +30,6 @@ Stocksituation = [
 
 choicelist = ['欠品','注文', '注意', '安全']
 df["在庫状況"] = np.select(Stocksituation, choicelist, default="Non")
-
 print(df)
 
 print("--------------------------------------------------------")
@@ -36,6 +40,7 @@ print(f"更新時間: {datetime.datetime.now()}")
 # print(df_1)
 # Ave_Max_Temple_Osa_Des = average(df_temperature_Osa["最高気温"])
 # print(f"12月の大阪平均気温(最高){Ave_Max_Temple_Osa_Des.round(2)}℃")
+
 MAX_TEMPERATURE = max(df["最高気温"])
 print(f"最高気温:{MAX_TEMPERATURE}℃")
 
@@ -53,7 +58,7 @@ Ave_Sales = average(df["売上個数"])
 print(f"平均売上個数{Ave_Sales.round(2)}個")
 
 # CSVに出力
-# df.to_csv("Sample.csv", encoding = "utf-8")
+df.to_csv("Sample.csv", encoding = "utf-8")
 
 
 
